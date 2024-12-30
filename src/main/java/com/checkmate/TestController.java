ac8b26b00c9bae9e3fe762f2fb48ead2b0594a5d
hyeon-gyu <hyungue729@naver.com>
2024-12-30 13:51:01 +0900

    Logback 라이브러리 적용 #15

--- src/main/java/com/checkmate/TestController.java
+++ src/main/java/com/checkmate/TestController.java
@@ -1,12 +1,22 @@
 package com.checkmate;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
-    @GetMapping
+    @GetMapping("/")
     public String test(){
         return "hello";
+    }
+
+    @GetMapping("/test")
+    public void errorTest() throws Exception {
+        log.info("info log");
+        log.warn("warn log");
+        log.error("error log");
+        throw new Exception("Error Test");
     }
 }
