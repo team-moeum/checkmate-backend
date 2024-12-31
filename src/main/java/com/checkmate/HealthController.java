a98f5dd52c07930e00e18f969a32fda81cc4c2b6
skysamer <skyslayer123@naver.com>
2024-12-31 13:51:49 +0900

    health 체커 api 수정
    - health 체커 api 수정

--- src/main/java/com/checkmate/HealthController.java
+++ src/main/java/com/checkmate/HealthController.java
@@ -1,9 +1,11 @@
 package com.checkmate;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/health")
+@RestController
+@RequestMapping("/health")
 public class HealthController {
     @GetMapping
     public String health(){
